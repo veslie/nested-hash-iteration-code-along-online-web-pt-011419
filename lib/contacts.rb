6b907852265809e9e360c97c447773f6733332b8
veslie losius <veslielosius@yahoo.com>
2019-02-12 12:55:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,12 +5,12 @@ require 'pry'
       email: "jon_snow@thewall.we", 
       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
     },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+    "Freddy Mercury" => {
+       name: "Freddy",
+      email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+  }
   
 def remove_strawberry(contacts)
 
